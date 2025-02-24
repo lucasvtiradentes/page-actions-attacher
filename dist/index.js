e0524d0c25658b234177973fde4252fbdcfa830a
Lucas Vieira <lucasvtiradentes@gmail.com>
2025-02-23 21:17:11 -0300

    chore: update commit message

--- dist/index.js
+++ dist/index.js
@@ -12,8 +12,8 @@
   };
   const libInfo = {
       name: 'webpage-customizer',
-      version: '1.12.4',
-      buildTime: '23/02/2025 21:12:49',
+      version: '1.12.5',
+      buildTime: '23/02/2025 21:17:12',
       link: 'https://github.com/lucasvtiradentes/webpage-customizer',
       temperMonkeyLink: 'https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo',
       initialScript: 'https://github.com/lucasvtiradentes/webpage-customizer/dist/initial_temper_monkey_script.js'
